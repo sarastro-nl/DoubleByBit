fc15d40faca50b6a2026834d9635e4b664d61875
sarastro-nl <23498997+sarastro-nl@users.noreply.github.com>
2023-10-12 11:43:13 +0200

    changed UInt to UInt64

--- main.swift
+++ main.swift
@@ -8,7 +8,7 @@ struct DoubleByBit: Equatable {
     var bytes: UInt64 = 0
 
     init(_ d: Double) { bytes = d.bitPattern }
-    init(_ s: String) { bytes = s.bitPattern }
+    init(_ s: String) { bytes = s.convertToUInt64() }
 
     private init(e: UInt64, m: UInt64, n: Bool = false) { bytes = e << 52 | m | (n ? 1 << 63 : 0) }
     
@@ -21,14 +21,8 @@ struct DoubleByBit: Equatable {
     var bitPatternString: String {
         var s = ""
         for i in (0...63).reversed() {
-            if bytes & 1 << i > 0 {
-                s += "1"
-            } else {
-                s += "0"
-            }
-            if i % 8 == 0 {
-                s += " "
-            }
+            s += ( bytes & 1 << i > 0 ) ? "1" : "0"
+            if i % 8 == 0 { s += " " }
         }
         return s
     }
@@ -245,7 +239,7 @@ struct DoubleByBit: Equatable {
     static func atan(_ operand: DoubleByBit) -> DoubleByBit {
         if operand == .zero { return .zero }
         if operand.isNegative { return -atan(-operand) }
-        let quarter = DoubleByBit(e: 1021, m: 0)
+        let quarter = DoubleByBit(e: 1021, m: 0)   // .0.25
         var m = DoubleByBit.zero
         var operand = operand
         while operand > quarter {
@@ -274,15 +268,15 @@ private extension String {
         let isNegative: Bool
     }
 
-    var bitPattern: UInt64 {
-        let powers: [UInt] = [
+    func convertToUInt64() -> UInt64 {
+        let powers: [UInt64] = [
             0,  3,  6,  9,  13, 16, 19, 23, 26, 29,
             33, 36, 39, 43, 46, 49, 53, 56, 59,
         ]
         var data = parse()
-        if data.nrDigits == 0 { return 0 }
-        var shift: UInt = 0
-        var exp: UInt = 1023
+        guard data.nrDigits > 0 else { return 0 }
+        var shift: UInt64 = 0
+        var exp: UInt64 = 1023
         while data.dp > 1 {
             shift = data.dp < 19 ? powers[data.dp] : 60
             rightShift(data: &data, shift: shift)
@@ -295,7 +289,7 @@ private extension String {
         }
         shift = 52
         let n = data.digits[..<3].reduce(0, { ($0 * 10) + UInt($1) })
-        var m: Int
+        var m: Int64
         if data.dp == 0 {  // between 0.1 and 1.0
             switch n {
                 case 100..<125: m = -4
@@ -311,36 +305,27 @@ private extension String {
                 default: m = 3
             }
         }
-        exp = m < 0 ? exp - UInt(-m) : exp + UInt(m)
-        shift = m < 0 ? shift + UInt(-m) : shift - UInt(m)
+        exp = m < 0 ? exp - UInt64(-m) : exp + UInt64(m)
+        shift = m < 0 ? shift + UInt64(-m) : shift - UInt64(m)
         leftShift(data: &data, shift: shift)
-        var mantisse: UInt64 = data.digits[..<16].reduce(0, { ($0 * 10) + UInt64($1) })
-        if data.nrDigits > 16 {
-            var roundUp = data.digits[16] >= 5
-            if data.digits[16] == 5 && data.nrDigits == 17 {
-                roundUp = 1 & data.digits[15] > 0
-            }
-            if roundUp {
-                mantisse += 1
-            }
-        }
-        return mantisse & (1 << 52 - 1) | UInt64(exp) << 52 | (data.isNegative ? 1 << 63 : 0)
+        let mantisse = data.digits[..<16].reduce(0, { ($0 * 10) + UInt64($1) }) - 1 << 52
+        return (data.isNegative ? 1 << 63 : 0) | exp << 52 | mantisse
     }
     
-    func leftShift(data: inout Data, shift: UInt) {
-        let extraDigits = [
+    func leftShift(data: inout Data, shift: UInt64) {
+        let extraDigitsTable = [
             0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4,
             4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8,
             8, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11,
             12, 12, 12, 13, 13, 13, 13, 14, 14, 14,
             15, 15, 15, 16, 16, 16, 16, 17, 17, 17,
             18, 18, 18, 19]
-        var extraDigit = extraDigits[Int(shift)]
-        var n: UInt = 0
+        var extraDigits = extraDigitsTable[Int(shift)]
+        var n: UInt64 = 0
         var ir = data.nrDigits - 1
-        var iw = data.nrDigits - 1 + extraDigit
+        var iw = data.nrDigits - 1 + extraDigits
         repeat {
-            n += UInt(data.digits[ir]) << shift; ir -= 1
+            n += UInt64(data.digits[ir]) << shift; ir -= 1
             data.digits[iw] = UInt8(n % 10); iw -= 1
             n /= 10
         } while ir >= 0
@@ -351,27 +336,26 @@ private extension String {
         if iw == 0 {
             data.digits.removeFirst()
             data.digits.append(0)
-            extraDigit -= 1
+            extraDigits -= 1
         }
-        data.nrDigits += extraDigit
-        data.dp += extraDigit
+        data.nrDigits += extraDigits
+        data.dp += extraDigits
         while data.digits[data.nrDigits - 1] == 0 {
             data.nrDigits -= 1
         }
     }
     
-    func rightShift(data: inout Data, shift: UInt) {
-        var n: UInt = 0
+    func rightShift(data: inout Data, shift: UInt64) {
+        var n: UInt64 = 0
         var ir = 0
         var iw = 0
         repeat {
-            n = (n * 10) + (ir < data.nrDigits ? UInt(data.digits[ir]) : 0); ir += 1
+            n = (n * 10) + (ir < data.nrDigits ? UInt64(data.digits[ir]) : 0); ir += 1
         } while n >> shift == 0
         data.dp -= ir - 1
-        let mask: UInt = 1 << shift - 1
         repeat {
             data.digits[iw] = UInt8(n >> shift); iw += 1
-            n = (n & mask) * 10 + (ir < data.nrDigits ? UInt(data.digits[ir]) : 0); ir += 1
+            n = (n & (1 << shift - 1)) * 10 + (ir < data.nrDigits ? UInt64(data.digits[ir]) : 0); ir += 1
         } while n > 0
         data.nrDigits = iw
     }
@@ -381,9 +365,9 @@ private extension String {
         guard let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) else { fatalError("Parse error: \(self)") }
         guard let match = regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) else { fatalError("Parse error: \(self)") }
         var dp = 0
+        var iw = 0
         var digits: [UInt8] = Array(repeating: 0, count: 1000)
         var isNegative = false
-        var iw = 0
         let string = self as NSString
         let signRange = match.range(withName: "sign")
         if signRange.length > 0, string.substring(with: signRange) == "-" {
